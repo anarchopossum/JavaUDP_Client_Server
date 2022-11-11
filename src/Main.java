a3c0c6558e125d89ba02be7bb0f37e804c5ce41d
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2022-11-10 22:14:18 -0800

    Server and Client Complete

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,49 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         /*
          *This is where the client will send data
          */
+        Scanner s = new Scanner(System.in);
+        // Asks the User to input Location, port, and message
+        System.out.println("Enter IP/Domain Name:");
+        String direction = s.nextLine();
+        System.out.println("Enter PortNumber:");
+        String portNum = s.nextLine();
+        System.out.println("Enter the Text you want to Send:");
+        String loveLetter = s.nextLine();
         // Creates a DatagramSocket
         DatagramSocket dataSock = new DatagramSocket();
-        // gets the machines address
-        InetAddress myAddy = InetAddress.getLocalHost();
         // Data that will be sent over the network
-        String loveLetter = "Never gonna give you up!";
         // changes the String to bytes to send over the network
         byte[] Translate = loveLetter.getBytes();
         // Sets up the DatagramPacket with (data,data length, my address, port number)
-        DatagramPacket Postman = new DatagramPacket(Translate, Translate.length, myAddy, 4200);
+        DatagramPacket Postman = new DatagramPacket(Translate, Translate.length, InetAddress.getByName(direction), Integer.parseInt(portNum));
         // Sends the packet
         dataSock.send(Postman);
+        System.out.println("Letter Sent");
 
         /*
          * This is where the client will be able to receive data
          */
-        // Creates a new byte that will hold the received data
-        byte[] reply = new byte[1024];
-        // setup for data being received
-        DatagramPacket box = new DatagramPacket(reply, reply.length);
-        // Actual package is received
-        dataSock.receive(box);
-        // Read the Data from the "box"
-        String readIt = new String(box.getData());
-        System.out.printf(readIt);
-        // Closes the Socket via timeout
-        dataSock.setSoTimeout(3000);
+         System.out.println("Waiting on Response...");
+         // Creates a new byte that will hold the received data
+         byte[] reply = new byte[1024];
+         // setup for data being received
+         DatagramPacket box = new DatagramPacket(reply, reply.length);
+         // Actual package is received
+         dataSock.receive(box);
+         // Read the Data from the "box"
+         String readIt = new String(box.getData());
+         System.out.printf(readIt);
+         // Closes the Socket via timeout
+         dataSock.setSoTimeout(3000);
+         System.out.println("\ndone!");
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,6 @@
-import java.lang.FdLibm.Pow;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.util.*;
+import java.util.Scanner;
 public class Server {
 
     public static void main(String[] args) throws Exception {
@@ -10,7 +8,11 @@ public class Server {
 		 * This is the reciving part of the server, Here clients will send data
 		*/
 		// Set Port for incoming info
-        DatagramSocket dataSock = new DatagramSocket(4200);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a Port Number: ");
+        int portVal = s.nextInt();
+		System.out.println("Waiting for message from Client...");
+        DatagramSocket dataSock = new DatagramSocket(portVal);
 		// Set incomming size of data
         byte[] inbox = new byte[1024];
 		// Sets datapacket info (the data, the size of the data)
@@ -19,15 +21,17 @@ public class Server {
         dataSock.receive(dataPack);
 		// stores the information from the datapacket
         String mail = new String(dataPack.getData());
-		
+		System.out.println("Recived Mail:\n"+mail);
+
 		/*
 		 * Sending Data Back
 		 */
-		int val = Integer.parseInt(mail.trim());
-		int sqr = val*val;
-		byte[] byte2 = (sqr+ "").getBytes();
-		InetAddress local = InetAddress.getLocalHost();
-		DatagramPacket letter = new DatagramPacket(byte2, byte2.length, local, dataPack.getPort());
-
+        String sendBack = mail.toUpperCase();
+        System.out.println("Sending back to: " + dataPack.getAddress());
+		DatagramPacket returnLetter = new DatagramPacket(sendBack.getBytes(),
+                mail.getBytes().length, dataPack.getAddress(), dataPack.getPort());
+        dataSock.send(returnLetter);
+        System.out.println("Letter Sent. :)");
+        dataSock.close();
     }
 }
